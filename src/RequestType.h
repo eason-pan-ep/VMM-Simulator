72d1745caf1c4a19d59e895ea3228f705cc547fd
eason-pan-ep <eason.pan@proton.me>
2023-11-09 11:00:01 -0800

    created the basic file structure for Workloads

---
+++ src/RequestType.h
@@ -0,0 +1,14 @@
+//
+// Created by Eason Pan on 2023-11-09.
+//
+
+#ifndef INC_5600_VVM_REQUESTTYPE_H
+#define INC_5600_VVM_REQUESTTYPE_H
+
+
+enum RequestType {
+
+};
+
+
+#endif //INC_5600_VVM_REQUESTTYPE_H
---
+++ src/SimRequest.h
@@ -0,0 +1,8 @@
+//
+// Created by Eason Pan on 2023-11-09.
+//
+
+#ifndef INC_5600_VVM_SIMREQUEST_H
+#define INC_5600_VVM_SIMREQUEST_H
+
+#endif //INC_5600_VVM_SIMREQUEST_H
